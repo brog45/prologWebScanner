27fb9aeb8bc25ea3a22615aee04bb3d87c6e6cba
Brian Rogers <brog45@gmail.com>
2019-03-24 01:30:10 -0500

    code to fuzz a post request

---
+++ postfuzz.pl
@@ -0,0 +1,52 @@
+:- use_module(library(http/http_client), [http_read_data/3, http_post/4]).
+:- use_module(library(http/http_header), [http_read_request/2]).
+:- use_module(library(url), [parse_url/2]).
+
+go :-
+    File = 'badstore-cartadd.txt',
+    file_request_form(File, Request, FormPairs),
+    request_to_url(Request, http, Url),
+    format('URL: ~w~nFORM: ~q~n', [Url, FormPairs]),
+    fuzz_loop(Url, FormPairs).
+
+fuzz_loop(Url, FormPairs) :- 
+    url_form_parameter_vulnerable(Url, FormPairs, ParameterName, Vulnerability), 
+    format('Possible ~w vulnerability in form parameter ~q~n', [Vulnerability, ParameterName]),
+    fail.
+fuzz_loop(_,_).
+
+file_request_form(File, Request, FormPairs) :-
+    setup_call_cleanup(open(File, read, Fd, []),
+        stream_request_form(Fd, Request, FormPairs),
+        close(Fd)).
+
+stream_request_form(Stream, Request, FormPairs) :-
+    http_read_request(Stream, Request), 
+    http_read_data(Request, FormPairs, []).
+
+request_to_url(Request, Protocol, Url) :-
+    subset([host(Host), path(Path), search(Search)], Request), 
+    parse_url(Url, [protocol(Protocol), host(Host), path(Path), search(Search)]).
+
+url_form_parameter_vulnerable(Url, FormPairs, ParameterName, Vulnerability) :-
+    proxy(Options), 
+    parse_url(Url, UrlAttributes),
+    vulnerability_spike(Vulnerability, Spike),
+    select(ParameterName=_, FormPairs, ParameterName=Spike, SpikedFormPairs),
+    append(UrlAttributes, [form(SpikedFormPairs)], NewRequest),
+    http_post(NewRequest, form(SpikedFormPairs), Reply, Options),
+    vulnerability_tell(Vulnerability, Tell),
+    sub_atom(Reply, _, _, _, Tell).
+
+vulnerability_spike(xss, 'fd<xss>sa').
+vulnerability_spike(sqli, 'fd\'sa').
+vulnerability_tell(xss, XssTell) :- vulnerability_spike(xss, XssTell).
+vulnerability_tell(sqli, 'error in your SQL syntax').
+
+proxy([proxy(Host:Port)]) :- 
+    getenv(http_proxy, Url), 
+    parse_url(Url, UrlAttributes), 
+    memberchk(host(Host), UrlAttributes), 
+    memberchk(port(Port), UrlAttributes), 
+    !.
+proxy([]).
