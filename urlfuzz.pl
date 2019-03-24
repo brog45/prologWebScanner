9ef62e13c4f013c1bb156db97fc092a2139f463c
Brian Rogers <brog45@gmail.com>
2019-03-24 01:44:23 -0500

    tweak rule ordering in urlfuzz.pl in anticipation of integrating URL and form fuzzing

--- urlfuzz.pl
+++ urlfuzz.pl
@@ -13,17 +13,17 @@ url_parameter_vulnerable(Url, Name, Vulnerability) :-
     vulnerability_tell(Vulnerability, Tell),
     sub_atom(ResponseBody, _, _, _, Tell).
 
-vulnerability_spike(xss, 'fd<xss>sa').
-vulnerability_spike(sqli, 'fd\'sa').
-vulnerability_tell(xss, XssTell) :- vulnerability_spike(xss, XssTell).
-vulnerability_tell(sqli, 'error in your SQL syntax').
-
 spike_url(Url, Spike, Name, SpikedUrl) :-
     parse_url(Url, Attributes),
     memberchk(search(Pairs), Attributes),
     select(Name=_, Pairs, Name=Spike, SpikedPairs),
     select(search(Pairs), Attributes, search(SpikedPairs), SpikedAttributes),
     parse_url(SpikedUrl, SpikedAttributes).
+
+vulnerability_spike(xss, 'fd<xss>sa').
+vulnerability_spike(sqli, 'fd\'sa').
+vulnerability_tell(xss, XssTell) :- vulnerability_spike(xss, XssTell).
+vulnerability_tell(sqli, 'error in your SQL syntax').
 
 proxy([proxy(Host:Port)]) :- 
     getenv(http_proxy, Url), 
